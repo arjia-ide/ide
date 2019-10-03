5ab08ea919af2238bcc1b2cc220486b3469f76b0
Roy van Kaathoven <ik@royvankaathoven.nl>
2019-10-03 15:15:54 +0200

    update solc files

--- src/widgets/editor/compiler/tronSolc.js
+++ src/widgets/editor/compiler/tronSolc.js
@@ -1,9 +1,7 @@
 // eslint-disable-next-line
 export const tronSolcGithubVersions = {
-  'tron-solc_0.4.25': 'https://cdn.jsdelivr.net/gh/TronSCS/tron-solc-bin/tron_0.4.25_Odyssey_v3.2.3.js',
-  'tron-solc_0.4.24': 'https://cdn.jsdelivr.net/gh/TRON-US/tronbox@64f9b0b95020c3f24e30bbb6b5061413514ab5b9/packages/tron-solc/soljson.js',
-  "tron-solc_v1":     "https://cdn.jsdelivr.net/gh/TRON-US/tronbox/packages/tron-solc/compiler-versions/soljson_v1.js",
-  "tron-solc_v2":     "https://cdn.jsdelivr.net/gh/TRON-US/tronbox/packages/tron-solc/compiler-versions/soljson_v2.js",
-  "tron-solc_v3":     "https://cdn.jsdelivr.net/gh/TRON-US/tronbox/packages/tron-solc/compiler-versions/soljson_v3.js",
-  "tron-solc_v4":     "https://cdn.jsdelivr.net/gh/TRON-US/tronbox/packages/tron-solc/compiler-versions/soljson_v4.js",
+  "tron-solc_0.4.24": "https://cdn.jsdelivr.net/gh/TRON-US/tronbox/packages/tron-solc/compiler-versions/soljson_v0.4.24.js",
+  "tron-solc_0.4.25": "https://cdn.jsdelivr.net/gh/TRON-US/tronbox/packages/tron-solc/compiler-versions/soljson_v0.4.25.js",
+  "tron-solc_0.5.4":  "https://cdn.jsdelivr.net/gh/TRON-US/tronbox/packages/tron-solc/compiler-versions/soljson_v0.5.4.js",
+  "tron-solc_0.5.8":  "https://cdn.jsdelivr.net/gh/TRON-US/tronbox/packages/tron-solc/compiler-versions/soljson_v0.5.8.js",
 };
