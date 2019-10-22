a3279ec040a4ade501a86256f5b14c73299b5196
Roy van Kaathoven <ik@royvankaathoven.nl>
2019-10-22 21:18:19 +0200

    update solc locations

--- src/widgets/editor/compiler/tronSolc.js
+++ src/widgets/editor/compiler/tronSolc.js
@@ -1,7 +1,8 @@
 // eslint-disable-next-line
 export const tronSolcGithubVersions = {
-  "tron-solc_0.4.24": "https://cdn.jsdelivr.net/gh/TRON-US/tronbox/packages/tron-solc/compiler-versions/soljson_v0.4.24.js",
-  "tron-solc_0.4.25": "https://cdn.jsdelivr.net/gh/TRON-US/tronbox/packages/tron-solc/compiler-versions/soljson_v0.4.25.js",
-  "tron-solc_0.5.4":  "https://cdn.jsdelivr.net/gh/TRON-US/tronbox/packages/tron-solc/compiler-versions/soljson_v0.5.4.js",
-  "tron-solc_0.5.8":  "https://cdn.jsdelivr.net/gh/TRON-US/tronbox/packages/tron-solc/compiler-versions/soljson_v0.5.8.js",
+  "tron-solc_0.4.24": "https://cdn.jsdelivr.net/gh/TRON-US/tron-solc-bin/bin/soljson_v0.4.24.js",
+  "tron-solc_0.4.25": "https://cdn.jsdelivr.net/gh/TRON-US/tron-solc-bin/bin/soljson_v0.4.25.js",
+  "tron-solc_0.5.4":  "https://cdn.jsdelivr.net/gh/TRON-US/tron-solc-bin/bin/soljson_v0.5.4.js",
+  "tron-solc_0.5.8":  "https://cdn.jsdelivr.net/gh/TRON-US/tron-solc-bin/bin/soljson_v0.5.8.js",
+  "tron-solc_0.5.9":  "https://cdn.jsdelivr.net/gh/TRON-US/tron-solc-bin/bin/soljson_v0.5.9.js",
 };
